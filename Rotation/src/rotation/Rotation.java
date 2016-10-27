98c45901f7541d19cbdd58460e91800ee9e437c8
Tim <Tim@kcsgaming.fritz.box>
2016-10-27 15:24:54 +0200

    -added Tofu -added FileReader

--- Rotation/src/rotation/Rotation.java
+++ Rotation/src/rotation/Rotation.java
@@ -5,17 +5,36 @@
  */
 package rotation;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author Tim
  */
 public class Rotation {
-
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // Ich versuche mal zu pushen
+    public static void main(String[] args) throws IOException {
+        /*
+         *  0. Einlesen
+         *  1. Check ob potentiell
+         *  2. Starte mit erstem Feld
+         *  3. Return Drehungen bzw. keine Drehung
+         */
+        try(BufferedReader br = new BufferedReader
+        (new FileReader("/rotation/rotation1.txt")
+        )){
+            String currentLine;
+            while((currentLine = br.readLine()) != null){
+                System.out.println(currentLine);
+            }
+         
+    
+        }
     }
     
 }
