bac120229d9985cb2f7e42e2fcd05c660c77ab28
arneturuc <arneturuc@gmail.com>
2016-10-27 20:22:39 +0200

    Edit Drehung

--- Rotation/src/rotation/Feld.java
+++ Rotation/src/rotation/Feld.java
@@ -30,9 +30,33 @@ public class Feld {
         switch (state) {
             case 0:
                 for (int n=0; n<data.length; n++) {
-                    for (int y=data.length-1; y>=n; y--) {
+                    for (int y=data.length-2; y>=n; y--) {
+                        int current = -1;
+                        boolean blocked = false;
                         for (int x=0; x<data.length; x++) {
-                            // Einzelnes Feld bewegen
+                            if (data[x][y] >= 0) {                              // Element
+                                if (data[x][y] != current) {                    // neues Element
+                                    if (blocked == false) {                     // altes Element nicht geblockt -> kopieren
+                                        for (int p=0; p<data.length; p++) {
+                                            if (data[p][y+1] == -5) {
+                                                // Wuhuu!
+                                            } else if (data[p][y+1] == -4) {    // verschiebe Element
+                                                data[p][y] = -4;
+                                                data[p][y+1] = current;
+                                            }
+                                        }
+                                    }
+                                    blocked = false;
+                                    current = data[x][y];
+                                }
+                                if (blocked==false && data[x][y+1]<-3) {        // nicht geblockt und frei
+                                    data[x][y+1] = data[x][y+1] + 2;            // erstelle temporären Wert
+                                } else blocked = true;                          // blockiert Element
+                            }
+                        }
+                        for (int p=0; p<data.length; p++) {                     // temporäre Werte löschen
+                            if (data[p][y+1]==-2 || data[p][y+1]==-3)
+                                data[p][y+1] = data[p][y+1] - 2;
                         }
                     }
                 }
