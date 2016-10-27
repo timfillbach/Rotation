2ddef3d09c00b7518a7a3b5a44cc365339d785e3
arneturuc <arneturuc@gmail.com>
2016-10-27 18:24:46 +0200

    Method applyGravity for Elements

--- Rotation/src/rotation/Feld.java
+++ Rotation/src/rotation/Feld.java
@@ -7,6 +7,7 @@ public class Feld {
     int state;
     
     public Feld(int[][] inputData, int inputState, boolean turnLeft) {
+        
         data = inputData;
         if (turnLeft) {
             if (inputState < 3) state++;
@@ -16,7 +17,30 @@ public class Feld {
             else state = 3;
         }
         
-        // More Stuff incoming
+        applyGravity();
+        
+        // Apply Gravity
+        // Testen ob erfolgreich
+        // Testen Abbruchbedingung
+        // Drehen Links
+        // Drehen Rechts
+    }
+    
+    private void applyGravity() {
+        switch (state) {
+            case 0:
+                for (int n=0; n<data.length; n++) {
+                    for (int y=data.length-1; y>=n; y--) {
+                        for (int x=0; x<data.length; x++) {
+                            // Einzelnes Feld bewegen
+                        }
+                    }
+                }
+                break; //Reihe von Unten, Unterste Reihe
+            case 1: break; //Spalte von Links
+            case 2: break; //Reihe von Oben
+            case 3: break; //Spalte von Rechts
+        }
     }
     
 }
